9a5e3abfad536e7f0fcecd9e216acadfc3e2c8cc
Justin Lս <lljustinlu@gmail.com>
2021-05-16 12:22:28 +1000

    made it better

--- index.js
+++ index.js
@@ -12,65 +12,42 @@ app.use(morgan('tiny', {stream: appLogStream}))
 app.use(express.static('build'))
 
 const Person = require('./models/person')
-// let persons = [
-//     { 
-//       "name": "Arto Hellas", 
-//       "number": "040-123456",
-//       "id": 1
-//     },
-//     { 
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523",
-//       "id": 2
-//     },
-//     { 
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345",
-//       "id": 3
-//     },
-//     { 
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122",
-//       "id": 4
-//     }
-//   ]
-var d = new Date()
-
-//adding a person
-// const generateId = () => {
-//     const maxId = persons.length > 0
-//     ? Math.max(...persons.map(p => p.id))
-//     : 0
-//     return Math.floor(Math.random() * (100 - maxId) + maxId)
-// }
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+    //check empty fields
     if(body.name == undefined || body.number == undefined){
+        
         return response.status(400).json({
             error: 'content missing'
         })
     }
-
-    const p = Person.find(person => person.name === body.name)
-
-    if(p){
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    } else{
-        const person = new Person({
+    //find if name already exists
+    Person.findOne({name:body.name})
+    .then(p => {
+        if(p){
+            return response.status(400).json({
+                error: 'cannot have the same name'
+            })
+        } else{
+            //make new person
+            const person = new Person({
             name: body.name,
             number: body.number
-        })
-    
-        // persons = persons.concat(person)
-        person.save().then(result => {
+            })
+
+            //save person to database
+            person.save().then(result => {
             response.json(result)
             console.log('person saved!')
-        })
-        
-    }
+            })
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(500).end()
+    })
+    
 })
 
 //searching for persons in the array
@@ -82,8 +59,6 @@ app.get('/api/persons', (request, response) => {
 
 //searching for an individual person
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
     const person = Person.findById(request.params.id)
     .then(
         person => {
@@ -93,25 +68,27 @@ app.get('/api/persons/:id', (request, response) => {
     .catch(()=>{
         response.status(404).end()
     })
-    // if(person){
-    //     response.json(person)
-    // } else{
-    //     response.status(404).end()
-    // }
 })
 
 //
 app.get('/info', (request, response) =>{
-
-    response.send('<p>Phonebook has info for ' + persons.length + ' people</p>' 
+    var d = new Date()
+    Person.count({}).then(
+        result => {
+            response.send('<p>Phonebook has info for ' + result + ' people</p>' 
     + d)
+        }
+    )
 })
 
 //Deleting a person
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    // persons = Person.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+    .then(result =>{
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+    
 })
 
 const PORT = process.env.PORT || 3001
